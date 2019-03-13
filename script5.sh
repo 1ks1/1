dac290782276ab32965f70762e74a51a1d901d15
1ks1 <48070238+1ks1@users.noreply.github.com>
2019-03-13 19:05:02 -0400

    Update script5.sh

--- script5.sh
+++ script5.sh
@@ -4,12 +4,20 @@ f_dir=$1
 d_dir=$2
 f_name=$3
 
-if [ -d $f_dir ]
-then
-find "$f_dir" -name "$f_name" -type f -exec cp {} $d_dir \;
-else
-mkdir $d_dir
-find "$f_dir" -name "$f_name" -type f -exec cp {} $d_dir \;
-fi
 
+if [[ -z $1 || -z $2 || -z $3 ]];
+then
+	echo "Warning! Empty parameters!" 
+	echo "scriptname <dir from> <dir to> <filename for>"
+else
+
+	if [ -d $f_dir ];
+		then
+		find "$f_dir" -name "$f_name" -type f -exec cp -f {} $d_dir \;
+	else
+		mkdir $d_dir
+		find "$f_dir" -name "$f_name" -type f -exec cp -f {} $d_dir \;
+	fi
+
+fi
  
