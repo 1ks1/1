cd8608ab68a5c0be82a554b74fe5551633b45c96
1ks1 <48070238+1ks1@users.noreply.github.com>
2019-03-13 19:55:05 -0400

    remove bad s links

---
+++ script6.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+f_dir=$1
+l_name=$2
+
+
+if [[ -z $1 || -z $2 ]];
+then
+	echo "Warning! Empty parameters!" 
+	echo "scriptname <dir from> <simlink name>"
+else
+
+	if [ -d $f_dir ];
+		then
+		for S in $(find "$f_dir" -type l); do
+			if [ ! -e "$(readlink $(basename $S))" ];
+			then
+				echo "$S - bad s link"
+				##rm "$S"
+			fi
+		done
+	else
+		echo "Not exists dir for"
+	fi
+
+fi
